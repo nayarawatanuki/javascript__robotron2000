28bcd800cd5021e0faf89254f48b31b11a5deb73
nayarawatanuki <nayarawatanuki@gmail.com>
2023-04-25 21:27:13 -0300

    project done

--- assets/js/components/skin.js
+++ assets/js/components/skin.js
@@ -2,5 +2,5 @@ import { robotron } from "../base/variables.js";
 
 export function changeSkin(color){
 
-    robotron.src = `assets/img/robotron200/skins/${color}/robotron2000.png`;
+    robotron.src = `assets/img/robotron2000/skins/${color}/robotron2000.png`;
  }
